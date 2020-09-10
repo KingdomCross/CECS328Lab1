3bb1e022b7984da2899df88252b015a5bcd504c0
KingdomCross <alexchheng1998@gmail.com>
2020-09-09 17:46:42 -0700

    Initial commit

---
+++ src/ModifiedInsertionSort.java
@@ -0,0 +1,59 @@
+/*
+Author: Alex Chheng
+Reference: https://www.journaldev.com/585/insertion-sort-java#:~:text=Insertion%20Sort%20in%20Java%201%20In%20java%20insertion,finally%20we%20have%20a%20sorted%20array%20of%20integers, https://www.informit.com/articles/article.aspx?p=680829&seqNum=3#:~:text=Processing%20a%20String%20One%20Character%20at%20a%20Time,from%200%20to%20the%20length%20of%20the%20string-1.
+ */
+
+import java.util.Arrays;
+
+public class ModifiedInsertionSort {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        stringCheck("Hello World");
+
+        int A[] = new int[10];
+        populateArray(A);
+        System.out.println("Before Sorting: ");
+        printArray(A);
+        // sort the array
+        insertionSort(A);
+        System.out.println("\nAfter Sorting: ");
+        printArray(A);
+    }
+
+    /**
+     * This method will sort the integer array using insertion sort in java algorithm
+     *
+     * @param arr
+     */
+    private static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int valueToSort = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] > valueToSort) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = valueToSort;
+        }
+    }
+
+    public static void printArray(int[] B) {
+        System.out.println(Arrays.toString(B));
+    }
+
+    public static void populateArray(int[] B) {
+        for (int i = 0; i < B.length; i++) {
+            B[i] = (int) (Math.random() * 100);
+        }
+    }
+
+    //Processing a String One Character at a Time
+    public static void stringCheck(String n){
+        for (int index = 0; index < n.length();
+             index++) {
+            char aChar = n.charAt(index);
+            System.out.println(aChar);
+        }
+    }
+}
