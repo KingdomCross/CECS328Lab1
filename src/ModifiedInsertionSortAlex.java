251a054a93f02e071f611c3cc435413d3f5baa83
KingdomCross <alexchheng1998@gmail.com>
2020-09-13 19:20:19 -0700

    CharCompare Function

--- src/ModifiedInsertionSortAlex.java
+++ src/ModifiedInsertionSortAlex.java
@@ -4,33 +4,27 @@ Author: Zixi Liu
 Reference: https://www.journaldev.com/585/insertion-sort-java#:~:text=Insertion%20Sort%20in%20Java%201%20In%20java%20insertion,finally%20we%20have%20a%20sorted%20array%20of%20integers, https://www.informit.com/articles/article.aspx?p=680829&seqNum=3#:~:text=Processing%20a%20String%20One%20Character%20at%20a%20Time,from%200%20to%20the%20length%20of%20the%20string-1.
  */
 
+import java.util.Scanner;
 import java.util.Arrays;
 
 public class ModifiedInsertionSortAlex {
-       public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        CharCompare("andyhavetest", "alexhavetest");
+
     	Scanner input = new Scanner(System.in);
         System.out.print("Enter a string of words: ");
         String[] original = input.nextLine().split("\\s+");
         String[] words = original.clone();  //get a clone of original array
         int[] counter = new int[original.length]; // get a empty Int array for counter
 
-        
-        
         sort(original, counter);
         
         System.out.println( " ");
 
-
-	 
-   	 for(int i = 1;i<words.length;i++)
-     {
+        for(int i = 1;i<words.length;i++) {
      	System.out.print(words[i] + " ");
      	System.out.println(counter[i] + " ");
-     	
-     }
-        
-        
+        }
     }
     
     public static void sort(String[] array, int[] counter)
@@ -61,4 +55,23 @@ public class ModifiedInsertionSortAlex {
          
     }
 
+    public static void CharCompare(String s1, String s2){
+        int keyLength;
+        if (s1.length() <= s2.length()){
+            keyLength = s1.length() - 1;
+        }
+        else {
+            keyLength = s2.length() - 1;
+        }
+
+        for (int index = 0; index < keyLength;
+             index++) {
+            char aChar = s1.charAt(index);
+            char bChar = s2.charAt(index);
+            if (aChar < bChar){
+                System.out.println(s1 + " " + s2);
+                System.out.println(aChar + " < " + bChar);
+            }
+        }
+    }
 }
